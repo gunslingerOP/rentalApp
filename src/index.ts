7afdf9c7f9ed4ad601b901b55bfb816ec54f5596
gunslinger <yahyahassan7219@yahoo.com>
2020-11-11 02:37:15 +0300

    db

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,29 @@ const port = config.port || 4000;
 import v1 from "../route/app/v1";
 import { Invoice } from "./entity/invoice";
 var cron = require("node-cron");
-
-
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 createConnection().then(async (connection) => {
   app.use(express.json());
 
+  
   app.use("/v1", v1);
   cron.schedule(" 0 0 * * *",async () => {
       let invoice:any;
