ae0cf4a98012f62bde5ac972bbfa6d7e3b7c46c7
gunslinger <yahyahassan7219@yahoo.com>
2020-11-11 00:49:50 +0300

    db

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ import { okRes } from "../helpers/tools";
 
 var cron = require("node-cron");
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 createConnection().then(async (connection) => {
   app.use(express.json());
 
@@ -33,7 +41,18 @@ createConnection().then(async (connection) => {
           }
       })
   });
-
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   app.listen(port, () => {
     console.log(`Running on port ${port}`);
   });
