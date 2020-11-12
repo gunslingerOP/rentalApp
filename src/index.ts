0c2365434d7cabd47aeb484f8324ccad00e14033
gunslinger <yahyahassan7219@yahoo.com>
2020-11-12 19:50:59 +0300

    installed cors

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,28 @@ import config from "../config/index";
 import * as express from "express";
 import { Connection, createConnection } from "typeorm";
 const app = express();
+var cors = require("cors");
 const port = process.env.PORT || 3000;
 import v1 from "../route/app/v1";
 import { Invoice } from "./entity/invoice";
 import { Property } from "./entity/property";
 var cron = require("node-cron");
 
-
 createConnection().then(async (connection) => {
   app.use(express.json());
+  app.use(cors());
 
   app.use("/v1", v1);
   cron.schedule(" 0 0 * * *", async () => {
     let invoice: any;
-let property:any;
+    let property: any;
     invoice = await Invoice.find({
       where: { Host_paid_status: false, paid_status: true },
     });
 
     property = await Property.find({
       where: { id: invoice.propertyId, booked: true },
-    })
+    });
     let d = new Date();
 
     let date = d.getDate();
@@ -31,13 +32,12 @@ let property:any;
       if (el.endDay == date && el.endMonth == month) {
         el.Host_paid_status = true;
         await el.save();
-        property.map(async(prop)=>{
-          prop.booked = false
-          await prop.save()
-        })
+        property.map(async (prop) => {
+          prop.booked = false;
+          await prop.save();
+        });
       }
     });
-    
   });
   app.listen(port, () => {
     console.log(`Running on port ${port}`);
